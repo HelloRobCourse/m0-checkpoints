f23f51dc0d3f55668e5a5c97cd7c210c51c50bd3
Jana Pavlasek <jbpavlasek@gmail.com>
2024-01-24 13:39:25 -0500

    Create calc_op.cpp

---
+++ calc_op.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    float num1, num2;  // Variables to store the numbers.
+    char op;  // Variable to store the operator.
+    
+    // Ask for the first number.
+    std::cout << "Please type a number and press enter: ";
+    std::cin >> num1;
+
+    // Ask for the operator.
+    std::cout << "Please type a math operator (one of: + - * or /): ";
+    std::cin >> op;
+
+    // Ask for the second number.
+    std::cout << "Please type another number and press enter: ";
+    std::cin >> num2;
+
+    // Write your code here! 
+    // Use the numbers and operator input by the user above to compute 
+    // the operation. To pass the tests, make sure you print out the full
+    // operation followed by the result (ex. 1 + 1 = 2).
+
+}
