4c9f03d349211fb1e46b66aa8426d99bbdc4b63d
Jana Pavlasek <jbpavlasek@gmail.com>
2024-01-24 13:50:01 -0500

    Create mathler.cpp

---
+++ mathler.cpp
@@ -0,0 +1,20 @@
+/////////////////////////
+// MATHLER in C++ //
+/////////////////////////
+
+// OBJECTIVE: Write a short C++ program that outputs an 8 character  
+// mathematical equation that equals 46.
+
+//  TODO:
+//  Write a C++ program here that does the following:
+ 
+// Write an assignment statement that will stores the result of a
+// formula that is _exactly_ 8 characters (no spaces) and is equal to 46.
+
+// Print to the terminal the equation for your formula as a string
+// (no spaces), an equals sign, then the result of the equation. Do
+// not just print 46!!
+// For example, this would be a valid solution for the number 13: 
+//   std::cout << "1+4/2+10=" << 1+4/2+10 << "\n";
+// This would be an illegal one (the real result is not 13!!):
+//   std::cout << "8-6*1/10=" << 13 << "\n";
