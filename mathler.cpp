f6badb444bc6ce4415d86cba3f2f68118da49362
Jana Pavlasek <jbpavlasek@gmail.com>
2024-01-24 16:22:34 -0500

    Update comment

--- mathler.cpp
+++ mathler.cpp
@@ -14,7 +14,8 @@
 // Print to the terminal the equation for your formula as a string
 // (no spaces), an equals sign, then the result of the equation. Do
 // not just print 46!!
-// For example, this would be a valid solution for the number 13: 
-//   std::cout << "1+4/2+10=" << 1+4/2+10 << "\n";
+// For example, this would be a valid solution for the number 13 if the 
+// result of the equation was stored in a variable called res: 
+//   std::cout << "1+4/2+10=" << res << "\n";
 // This would be an illegal one (the real result is not 13!!):
 //   std::cout << "8-6*1/10=" << 13 << "\n";
